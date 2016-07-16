2bf24ba12348eee2e0f1eb067450f41ec34161d1
Malyssa Winters <malyssawinters5@gmail.com>
2016-07-16 13:04:32 -0400

    complete project

---
+++ Bricks Game/Assets/BallMove.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallMove : MonoBehaviour {
+
+    private Rigidbody2D ballForce;
+    public float force;
+
+    // Use this for initialization
+    void Start () {
+        ballForce = GetComponent<Rigidbody2D>();
+        ballForce.AddForce (new Vector2(1f, 0.5f) * Time.deltaTime * force);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    }
+}
---
+++ Bricks Game/Assets/Move.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+    Vector2 position;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        position = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float moveX = Input.GetAxis("Horizontal");
+        position.x += moveX * Time.deltaTime * speed;
+        position.x = Mathf.Clamp(position.x, -8f, +8f);
+        transform.position = position;
+	}
+}
